19569872b71bfaff94a6f530bfbbd57248fd6738
github4karen <github4karen@gmail.com>
2017-04-16 12:42:43 -0600

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,20 @@
+## Get the complete dataset
+data_all  <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+## Format date to Type Date
+data_all$Date <- as.Date(data_all$Date, format="%d/%m/%Y")
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+data <- subset(data_all, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+
+## create plot type=line, with axis lables
+with(data, plot(V1,V3, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+## Saving to file
+dev.copy(png, file="plot4.png", height=480, width=480)
+## Closing Graphics Device
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,37 @@
+## Get the complete dataset
+data_all  <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+## Format date to Type Date
+data_all$Date <- as.Date(data_all$Date, format="%d/%m/%Y")
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+data <- subset(data_all, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+###### Begin Plot 3
+with(data, {
+    plot(Sub_metering_1~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+})
+##add legend
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+## Closing Graphics Device
+dev.off()
+
+###### Begin Plot 1
+## histogarm with main title and axis labels
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+## Closing Graphics Device
+dev.off()
